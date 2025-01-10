928132c38081877522f138e2ede4eb60d4cf79d5
Máximo Fernández Riera <43608040+maximofernandezriera@users.noreply.github.com>
2025-01-10 11:20:36 +0000

    Subo los cambios del equipo 1

--- app/src/main/java/codespaces/blank/WallyFinder.java
+++ app/src/main/java/codespaces/blank/WallyFinder.java
@@ -1,10 +1,18 @@
 package codespaces.blank;
 
 public class WallyFinder {
+  public static int whereIsWally(String str) {
+      // Convertimos la cadena en un array de caracteres
+      char[] chars = str.toCharArray();
 
-    public static int wheresWally(String str) {
-      // Find Wally here
+      // Buscamos la primera aparición de "Wally" en el array, ojo que Wally tiene una longitud de 4
+      for (int i = 0; i < chars.length - 5; i++) {
+          if (chars[i] == 'W' && chars[i + 1] == 'a' && chars[i + 2] == 'l' && chars[i + 3] == 'l' && chars[i + 4] == 'y') {
+              return i;
+          }
+      }
+
+      // Si no se encuentra "Wally", devolvemos -1
       return -1;
-    }
-  
   }
+}
